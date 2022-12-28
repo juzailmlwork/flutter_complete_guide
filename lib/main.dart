30f848716374bf60dc0837dab768caf6bef9a38f
Mohamed Jusail <juzailmlwork@gmail.com>
2022-12-28 19:34:35 +0530

    added button to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Title(color: Colors.green, child: Text("Welcome")),
-        ),
-        body: Text("My name is Jusail"),
-      ),
+          appBar: AppBar(
+            title: Title(color: Colors.green, child: Text("Welcome")),
+          ),
+          body: Column(
+            children: [
+              Text("I am Jusail"),
+              Text("How are you"),
+              ElevatedButton(onPressed: null, child: Text("press me"))
+            ],
+          )),
     );
   }
 }
